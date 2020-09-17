2f367b68046d81677e6977b14e2b5851e99ba2be
kausta <kausta@ttu.ee>
2020-09-17 19:35:45 +0000

    Resolve "Set up backend"

---
+++ src/main/java/ee/taltech/cars/Application/CarsApplication.java
@@ -0,0 +1,17 @@
+package ee.taltech.cars.Application;
+
+import ee.taltech.cars.controller.IndexController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = IndexController.class)
+public class CarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsApplication.class, args);
+    }
+
+
+}
