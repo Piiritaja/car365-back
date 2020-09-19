835716a646a3d57c56a471df2616da13a953724e
Kaspar Ustav <kausta@ttu.ee>
2020-09-19 22:04:16 +0300

    Initial IndexController

--- src/main/java/ee/taltech/cars/controller/IndexController.java
+++ src/main/java/ee/taltech/cars/controller/IndexController.java
@@ -7,7 +7,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping
-    public String index() {
-        return "Congrats, you got everything up and running!";
+    public String getIndex() {
+        return "Landing page.\n";
+    }
+
+    @GetMapping("sign_up")
+    public String getSignUp() {
+        return "Sign up page";
+    }
+
+    @GetMapping("log_in")
+    public String getLogIn() {
+        return "Login page";
+    }
+
+    @GetMapping("contact")
+    public String getContact() {
+        return "Contact page";
+    }
+
+    @GetMapping("car")
+    public String getCar() {
+        return "Cars page";
     }
 }
