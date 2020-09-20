f8f8ec051b1a407ad6ab6c67389c18942dc75d9e
Kaspar Ustav <kausta@ttu.ee>
2020-09-21 00:06:43 +0300

    Added initial Listing model
    Signed-off-by: Kaspar Ustav <kausta@ttu.ee>

---
+++ src/main/java/ee/taltech/cars/models/Listing.java
@@ -0,0 +1,17 @@
+package ee.taltech.cars.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+public class Listing {
+    private final String id;
+    @Setter
+    private String title;
+    @Setter
+    private String description;
+    private final User owner;
+    private final Car listedCar;
+}
