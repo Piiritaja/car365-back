0968a435594f8a4654aa4d9452f087ce9a8b6da3
Kaspar Ustav <kausta@ttu.ee>
2020-09-21 00:04:12 +0300

    Initial User model
    Signed-off-by: Kaspar Ustav <kausta@ttu.ee>

--- src/main/java/ee/taltech/cars/models/User.java
+++ src/main/java/ee/taltech/cars/models/User.java
@@ -2,9 +2,19 @@ package ee.taltech.cars.models;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Singular;
+
+import java.util.List;
 
 @Builder
 @Getter
 public class User {
+    private final String id;
+    private final String firstName;
+    private final String LastName;
+    @Singular
+    private final List<Car> cars;
+
+
 
 }
