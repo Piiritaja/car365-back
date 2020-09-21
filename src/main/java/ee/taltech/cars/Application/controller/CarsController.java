afc369025d3c686d4f066f58bdee0fca7520f2ee
mikk järvis <mikjar@ttu.ee>
2020-09-21 22:13:07 +0300

    #6 updated get request for "/cars" && added CarNotFoundException

--- src/main/java/ee/taltech/cars/Application/controller/CarsController.java
+++ src/main/java/ee/taltech/cars/Application/controller/CarsController.java
@@ -1,13 +1,24 @@
 package ee.taltech.cars.Application.controller;
 
+import ee.taltech.cars.Application.exception.CarNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("cars")
 @RestController
 public class CarsController {
+    private List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
-    @GetMapping("/cars")
-    public String getCars() {
-        return "Cars page";
+    @GetMapping("{id}")
+    public String getCars(@PathVariable Integer id) {
+        return String.format("Car: %s",
+                numbers.stream()
+                        .filter(nr -> nr.equals(id))
+                        .findAny().orElseThrow(CarNotFoundException::new).toString());
     }
+
 }
