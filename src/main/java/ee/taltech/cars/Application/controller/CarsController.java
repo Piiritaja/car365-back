baeb6f91e813b7574ab380a13f0d3d06ada2e351
mikk järvis <mikjar@ttu.ee>
2020-09-21 22:15:29 +0300

    #6 added post request to add cars to "database"

--- src/main/java/ee/taltech/cars/Application/controller/CarsController.java
+++ src/main/java/ee/taltech/cars/Application/controller/CarsController.java
@@ -3,6 +3,8 @@ package ee.taltech.cars.Application.controller;
 import ee.taltech.cars.Application.exception.CarNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,8 @@ import java.util.List;
 @RequestMapping("cars")
 @RestController
 public class CarsController {
+
+    //TODO use database instead of list
     private List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
     @GetMapping("{id}")
@@ -21,4 +25,12 @@ public class CarsController {
                         .findAny().orElseThrow(CarNotFoundException::new).toString());
     }
 
+    @PostMapping
+    public String saveCar(@RequestBody Integer id) {
+        if (!numbers.contains(id)) {
+            numbers.add(id);
+            return "Added: " + id;
+        }
+        return "ID already exists!";
+    }
 }
