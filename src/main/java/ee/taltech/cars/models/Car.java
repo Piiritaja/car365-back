ad0794aa0766058e485feeb1d7adb97aa58c90ca
mikk järvis <mikjar@ttu.ee>
2020-09-21 14:42:54 +0300

    #11 added color attribute to car

--- src/main/java/ee/taltech/cars/models/Car.java
+++ src/main/java/ee/taltech/cars/models/Car.java
@@ -10,14 +10,15 @@ import lombok.Getter;
 public class Car {
     private final @Builder.Default
     String id = java.util.UUID.randomUUID().toString();
-    private final User owner;
     private final String bodyType;
     private final String brand;
     private final String model;
-    private final int releaseYear;
+    private final String color;
     private final String gearboxType;
     private final String fuelType;
+    private final String driveType;
     private final int enginePower;
     private final int mileage;
-    private final String driveType;
+    private final int releaseYear;
+    private final User owner;
 }
