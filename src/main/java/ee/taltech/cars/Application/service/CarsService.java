118d60fc62675be532b9997e03342a9b652bcd7a
mikk järvis <mikjar@ttu.ee>
2020-09-22 14:18:12 +0300

    #15 added Cars service && update Car model accordingly

---
+++ src/main/java/ee/taltech/cars/Application/service/CarsService.java
@@ -0,0 +1,49 @@
+package ee.taltech.cars.Application.service;
+
+import ee.taltech.cars.Application.exception.CarNotFoundException;
+import ee.taltech.cars.models.Car;
+import ee.taltech.cars.repository.CarsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarsService {
+
+    @Autowired
+    private CarsRepository carsRepository;
+
+    public List<Car> findAll() {
+        return carsRepository.findAll();
+    }
+
+    public Car findById(Long id) {
+        return carsRepository.findById(id).orElseThrow(CarNotFoundException::new);
+    }
+
+    public Car save(Car car) {
+        return carsRepository.save(car);
+    }
+
+    public Car update(Car car, Long id) {
+        Car dbCar = findById(id);
+        dbCar = new Car(dbCar.getId(),
+                car.getBodyType(),
+                car.getBrand(),
+                car.getModel(),
+                car.getColor(),
+                car.getGearboxType(),
+                car.getFuelType(),
+                car.getDriveType(),
+                car.getEnginePower(),
+                car.getMileage(),
+                car.getReleaseYear(),
+                car.getOwner());
+        return carsRepository.save(dbCar);
+    }
+
+    public void delete(Long id) {
+        carsRepository.delete(findById(id));
+    }
+}
--- src/main/java/ee/taltech/cars/models/Car.java
+++ src/main/java/ee/taltech/cars/models/Car.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Builder
 @EqualsAndHashCode
 public class Car {
-    private final @Builder.Default
+    private @Builder.Default
     String id = java.util.UUID.randomUUID().toString();
     private final String bodyType;
     private final String brand;
@@ -21,4 +21,20 @@ public class Car {
     private final int mileage;
     private final int releaseYear;
     private final User owner;
+
+    public Car(String id, String bodyType, String brand, String model, String color, String gearboxType, String fuelType,
+               String driveType, int enginePower, int mileage, int releaseYear, User owner) {
+        this.id = id;
+        this.bodyType = bodyType;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.gearboxType = gearboxType;
+        this.fuelType = fuelType;
+        this.driveType = driveType;
+        this.enginePower = enginePower;
+        this.mileage = mileage;
+        this.releaseYear = releaseYear;
+        this.owner = owner;
+    }
 }
