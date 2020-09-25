94a34eb89f3533f9d47eee8d4b05b8458c44aed7
Sedrik Suurmets <sedrik.suurmets@gmail.com>
2020-09-25 11:13:21 +0300

    #13 user -> owner. Fixed postgre errors

--- src/main/java/ee/taltech/cars/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.taltech.cars.repository;
-
-import ee.taltech.cars.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
