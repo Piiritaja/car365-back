92e68bb2c4412f812a7637bf0dc7d493b959855a
Kaspar Ustav <kausta@ttu.ee>
2020-09-26 22:18:33 +0300

    Put and post methods for ListingController. Added custom exceptions for invalid listings.
    Signed-off-by: Kaspar Ustav <kausta@ttu.ee>

--- src/main/java/ee/taltech/cars/controller/ListingController.java
+++ src/main/java/ee/taltech/cars/controller/ListingController.java
@@ -4,9 +4,7 @@ package ee.taltech.cars.controller;
 import ee.taltech.cars.models.Listing;
 import ee.taltech.cars.service.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +19,21 @@ public class ListingController {
     public List<Listing> getAll() {
         return listingService.findAll();
     }
+
+    @GetMapping("{id}")
+    public Listing getById(@PathVariable String id) {
+        return listingService.findById(id);
+    }
+
+    @PutMapping("{id}")
+    public Listing putById(@PathVariable String id) {
+        return listingService.update(listingService.update(listingService.findById(id)));
+    }
+
+    @PostMapping
+    public Listing postListing(Listing listing) {
+        return listingService.save(listing);
+    }
+
+
 }
--- src/main/java/ee/taltech/cars/service/ListingService.java
+++ src/main/java/ee/taltech/cars/service/ListingService.java
@@ -1,5 +1,8 @@
 package ee.taltech.cars.service;
 
+import ee.taltech.cars.IdValidator;
+import ee.taltech.cars.controller.ListingNotFoundException;
+import ee.taltech.cars.exception.InvalidListingException;
 import ee.taltech.cars.models.Listing;
 import ee.taltech.cars.repository.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +12,32 @@ import java.util.List;
 
 @Service
 public class ListingService {
+    IdValidator validator = new IdValidator();
+
 
     @Autowired
     private ListingRepository listingRepository;
 
     public List<Listing> findAll() {
         return listingRepository.findAll();
+    }
+
+    public Listing findById(String id) {
+        if (validator.validate(id)) return listingRepository.findById(id).orElseThrow(ListingNotFoundException::new);
+        throw new InvalidListingException();
+    }
+
+    public Listing save(Listing listing) {
+        if (validator.validate(listing.getId())) return listingRepository.save(listing);
+        throw new InvalidListingException();
+    }
+
+    public void delete(String id) {
+        if (validator.validate(id)) listingRepository.delete(findById(id));
+        throw new InvalidListingException();
+    }
+
+    public Listing update(Listing listing) {
+        return save(listing);
     }
 }
