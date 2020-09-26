9225741faad82954a54a206a5b3857a43a13bc81
Kaspar Ustav <kausta@ttu.ee>
2020-09-26 23:12:49 +0300

    Mock listings, owners and cars
    Signed-off-by: Kaspar Ustav <kausta@ttu.ee>

--- src/main/java/ee/taltech/cars/CarsApplicationInit.java
+++ src/main/java/ee/taltech/cars/CarsApplicationInit.java
@@ -1,7 +1,11 @@
 package ee.taltech.cars;
 
 import ee.taltech.cars.models.Car;
+import ee.taltech.cars.models.Listing;
+import ee.taltech.cars.models.Owner;
 import ee.taltech.cars.repository.CarsRepository;
+import ee.taltech.cars.repository.ListingRepository;
+import ee.taltech.cars.repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,27 +15,65 @@ public class CarsApplicationInit implements CommandLineRunner {
     @Autowired
     CarsRepository carsRepository;
 
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    @Autowired
+    ListingRepository listingRepository;
+
     public void mockCars() {
         for (int i = 1; i < 50; i++) {
-            Car car = new Car();
-            car.setReleaseYear(1990 + i);
-            car.setBodyType("Body type " + i);
-            car.setBrand("Brand " + i);
-            car.setColor("Color " + i);
-            car.setDriveType(i + "wheel drive");
-            car.setEnginePower(300 + i);
-            car.setGearboxType("Automatic");
-            car.setMileage(1200 + (i * 3));
-            car.setFuelType("Disel");
-            car.setModel("Model " + i);
+            Car car = Car.builder()
+                    .releaseYear(1990 + i)
+                    .bodyType("Body type " + i)
+                    .brand("Brand " + i)
+                    .color("Color " + i)
+                    .driveType(i + "wheel drive")
+                    .enginePower(300 + i)
+                    .gearboxType("Automatic")
+                    .gearboxType("Automatic")
+                    .mileage(1200 + (i * 3))
+                    .fuelType("Disel")
+                    .model("Model " + i)
+                    .build();
             carsRepository.save(car);
+        }
+    }
+
+    public void mockUsers() {
+        for (int i = 1; i < 50; i++) {
+            Owner owner = Owner.builder()
+                    .firstName("Sample")
+                    .lastName("User nr." + i)
+                    .build();
+            ownerRepository.save(owner);
+        }
+    }
+
+    public void mockListings() {
+        for (int i = 1; i < 50; i++) {
+            Listing listing = Listing
+                    .builder()
+                    .title("Listing " + i)
+                    .description("It is a cool car")
+                    .location("Tallinn")
+                    .price(5000 + (i * 5))
+                    .status("In a relationship")
+                    .build();
+            listingRepository.save(listing);
+
+
         }
     }
 
     @Override
     public void run(String... args) throws Exception {
         carsRepository.deleteAll();
+        ownerRepository.deleteAll();
+        listingRepository.deleteAll();
         mockCars();
+        mockUsers();
+        mockListings();
 
     }
 }
