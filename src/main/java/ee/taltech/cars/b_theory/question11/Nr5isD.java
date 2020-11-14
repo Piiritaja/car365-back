6855fdd377348443dd2d46f6616e47ff72e26ddb
Mikk Järvis <mikjar@ttu.ee>
2020-11-14 15:20:14 +0200

    TheoryB Q11-D + removed unnecessary class

--- src/main/java/ee/taltech/cars/b_theory/question11/Nr5isD.java
+++ src/main/java/ee/taltech/cars/b_theory/question11/Nr5isD.java
@@ -1,8 +1,67 @@
-package b_theory.question11;
+package ee.taltech.cars.b_theory.question11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Nr5isD {
 
     //todo this is a contribution based question so make sure to keep commits separate
     //todo A What does D stand for in SOLID? Explain the principle.
-    //todo B Give an example. Write actual or pseudo code.
+    //  Answer: D stands for Dependency inversion principle
+    //  This means that classes shouldn't be dependent on multiple other classes directly. Rather they should be dependent
+    //  on in-between layer (like abstract class) from which other classes are dependent on. It makes code more expendable.
 }
+
+//todo B Give an example. Write actual or pseudo code.
+// this is an example without Dependency inversion
+class Teacher {
+    private List<IAIB> iaibStudents = new ArrayList<>();
+    private List<IAAB> iaabStudents = new ArrayList<>();
+
+    public void addIAIB(IAIB student) {
+        this.iaibStudents.add(student);
+    }
+
+    public void addIAAB(IAAB student) {
+        this.iaabStudents.add(student);
+    }
+
+    public List<IAIB> getIAIB() {
+        return iaibStudents;
+    }
+
+    public List<IAAB> getIAAB() {
+        return iaabStudents;
+    }
+}
+
+//todo this is example with Dependency inversion, using abstract class Student, we can only use only one list, get and
+// add methods. Without modifying Teacher (s)he can teach IABB or anyother StudyProgramme
+
+class TeacherD {
+    private List<StudyProgramme> students = new ArrayList<>();
+
+    public void addStudent(StudyProgramme student) {
+        this.students.add(student);
+    }
+
+    public List<StudyProgramme> getStudents(Class<? extends StudyProgramme> studyProgramme) {
+        return students.stream().filter(student -> student.getClass() == studyProgramme).collect(Collectors.toList());
+    }
+}
+
+abstract class StudyProgramme {
+}
+
+class IAIB extends StudyProgramme {
+}
+
+class IAAB extends StudyProgramme {
+}
+
+class IABB extends StudyProgramme {
+}
+
+
+
