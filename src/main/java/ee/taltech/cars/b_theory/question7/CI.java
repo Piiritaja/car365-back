85a7bbeefbbb44125b5d272a66559eef4bc81518
Kaspar Ustav <kausta@ttu.ee>
2020-11-14 03:20:18 +0200

    theory q7
    Signed-off-by: Kaspar Ustav <kausta@ttu.ee>

--- src/main/java/ee/taltech/cars/b_theory/question7/CI.java
+++ src/main/java/ee/taltech/cars/b_theory/question7/CI.java
@@ -4,7 +4,7 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: combining software parts into one system.
 
     //todo B
     // What makes integration continuous?
@@ -12,20 +12,21 @@ public class CI {
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
-    // [] test
+    // [x] compile
+    // [x] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // Testing. When contributing to a project, you want to be sure that the changes you made, do not brake the functionality of the software.
+    // Also this way, providing you have meaningful tests, the underlying functionality of the software stays the same, meaning someone can not on their own make fundamental changes to the software.
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Initial time cost. Setting up automated testing, writing meaningful tests, setting up runners can initially take a lot of time.
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Jenkins
+    // 2 Github (Github actions)
 }
