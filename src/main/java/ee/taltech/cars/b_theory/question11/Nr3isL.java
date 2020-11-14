9ca47ad96f251b6c7a20b92e8e377ed4deedff2b
Ewert Ubaleht <ewubal@ttu,ee>
2020-11-14 15:48:23 +0200

    theory question 11, nr3

--- src/main/java/ee/taltech/cars/b_theory/question11/Nr3isL.java
+++ src/main/java/ee/taltech/cars/b_theory/question11/Nr3isL.java
@@ -1,8 +1,71 @@
 package ee.taltech.cars.b_theory.question11;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Nr3isL {
 
     //todo this is a contribution based question so make sure to keep commits separate
     //todo A What does L stand for in SOLID? Explain the principle.
+    // Answer: L stands for Liskov Substitution principle. The principle defines that objects of a superclass shall be
+    // replaceable with objects of its subclasses without breaking the application.
+    // That requires the objects of subclasses to behave in the same way as the objects of superclass.
     //todo B Give an example. Write actual or pseudo code.
+
 }
+//todo Let's say we have a class Vehicle, that has name and engine.
+@Getter
+@Setter
+class Vehicle {
+
+    String name;
+    String engine;
+
+    void startEngine(String engine) { }
+}
+
+//todo And we have class Car that extends Vehicle
+class Car extends Vehicle {
+
+    @Override
+    void startEngine(String engine) { }
+}
+
+//todo But if we want to add bicycle, we couldn't because bicycles do not have engine
+// so we can't implement startEngine method.
+// So we add new more specified classes that extend vehicle.
+
+@Getter
+@Setter
+class VehicleAfter {
+    String name;
+}
+
+@Getter
+@Setter
+class VehiclesWithEngine extends VehicleAfter {
+
+    String engine;
+
+    void startEngine(String engine) { }
+}
+
+class CarAfter extends VehiclesWithEngine {
+
+    @Override
+    void startEngine(String engine) { }
+}
+
+class VehiclesWithoutEngine extends VehicleAfter {
+
+    void startMoving() { }
+}
+
+class Bicycle extends VehiclesWithoutEngine {
+
+    @Override
+    void startMoving() { }
+}
+
+//todo And now it enables us to replace objects of a parent
+// class with objects of a subclass without braking anything.
