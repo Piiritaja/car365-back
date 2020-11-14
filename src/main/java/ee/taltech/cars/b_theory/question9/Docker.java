1404469cf4960e54b2989801a38a67493df0b705
Ewert Ubaleht <ewubal@ttu,ee>
2020-11-14 18:41:18 +0200

    theory question 9

--- src/main/java/ee/taltech/cars/b_theory/question9/Docker.java
+++ src/main/java/ee/taltech/cars/b_theory/question9/Docker.java
@@ -1,27 +1,34 @@
-package b_theory.question9;
+package ee.taltech.cars.b_theory.question9;
 
 public class Docker {
 
     //todo A
     // What is a server?
-    // Answer:
+    // Answer: Server is a computer program or device that provides a service
+    // to another computer program and its client. Servers can provide many functionalities,
+    // like sharing data or resources among multiple clients, or performing computation for a client.
 
     //todo B
     // What is the difference between build server and production server?
-    // Answer:
+    // Answer: Difference is mainly in security. Usually build server allows unrestricted access to and
+    // control by a user or group of users. A production server is configured to restrict access to
+    // authorized users and to limit control to system administrators.
 
     //todo C
     // What is docker?
-    // Answer:
+    // Answer:Docker is a platform for developing, shipping and running applications.
+    // It provides the ability to package and run an application in a loosely isolated environment called a container.
 
     //todo D
     // Name and explain docker container benefits over virtual machine setup (java jar as system process and installed nginx)
-    // 1
-    // 2
+    // 1 With a container environment, multiple workloads can run with 1 OS.
+    // 2 Containers are typically much smaller, faster and more flexible, which makes them much
+    // better fit for fast development cycles, microservices and its easier to update them(containers).
 
     //todo E
     // Name and explain docker container drawback over virtual machine setup (java jar as system process and installed nginx)
-    // 1
+    // 1 Container uses the kernel of th host OS and has operating system dependencies.
+    // Therefore, containers can differ from the underlying OS by dependency, but not by type.
 
     //todo F
     // Name and describe tools for docker architecture
