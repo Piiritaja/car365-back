3c758befaa6663483126716880bc69a05b2231da
Sedrik Suurmets <sesuur@ttu.ee>
2020-12-05 18:12:49 +0200

    #52 add questions and question nr 12 first part done

---
+++ src/main/java/ee/taltech/cars/c_theory/question12/Security.java
@@ -0,0 +1,25 @@
+package ee.taltech.cars.c_theory.question12;
+
+public class Security {
+
+    //todo A
+    // What is authentication?
+    // Answer:
+    // Identifying if a client (user) or a server are who they claim to be.
+    // For example by logging in the user gives a password and an identifier (username or email etc).
+    // If both are correct and match the user,
+    // the user is authenticated by the server to be who they claim to be.
+
+    //todo B
+    // What is authorization?
+    // Answer:
+    // Giving someone access to system features according their privileges (ex. User or Admin).
+    // User could be authorized to view posts on a page and an admin could be authorized to view and make new posts.
+
+    //todo C
+    // For web (HTTP backend) Spring Security is configured as a ... ? Explain it.
+    // Think or read how Spring Security checks are injection into your application.
+    // Configured as (one word): filter??????????????
+    // Description:
+
+}
